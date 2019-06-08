683d27800fa958edf2587324f819521e45554dcb
kyle mcarthur <kmcarthur@mail.weber.edu>
2019-06-08 10:12:28 -0600

    Initial Commit

---
+++ Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory
+//
+//  Created by Kyle McArthur on 6/8/19.
+//  Copyright © 2019 Kyle McArthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
